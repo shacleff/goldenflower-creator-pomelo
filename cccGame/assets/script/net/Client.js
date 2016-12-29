132a24312fae92a055de5510699f8f715aeec076
gjh1987 <605825665@qq.com>
2016-12-29 18:18:20 +0800

    update...........

--- cccGame/assets/script/net/Client.js
+++ cccGame/assets/script/net/Client.js
@@ -1,3 +1,24 @@
 /**
  * Created by Administrator on 2016/12/27.
  */
+Core.$Defines("Client")({
+    onrecv: null,
+    _default: function(a) {
+        delete a.payloadName;
+        cc.log("CMD NOT SUPPORT: ", a)
+    },
+    addmap: function(a, c, b, d) {
+        throw "addmap: Client not init.";
+    },
+    removemap: function(a, c) {},
+    onsyserror: function(a, c) {
+        if (this.onrecv) this.onrecv();
+        cc.log("net-Js-error:" + a + "\nfile:" + a.fileName, " line:" + a.lineNumber, " cols:" + a.columnNumber, "\n ", String(c))
+    },
+    onservererror: function(a, c) {
+        cc.log("server err:" + a)
+    },
+    onbussinesserror: function(a) {
+        cc.log("onbussinesserror error:ID ", a.ID, " errorcode:", a.errorCode)
+    }
+});
--- cccGame/assets/script/net/Server.js
+++ cccGame/assets/script/net/Server.js
@@ -1,3 +1,67 @@
 /**
  * Created by Administrator on 2016/12/27.
  */
+Core.$Defines("Server")({
+    onsent: null,
+    onopen: null,
+    state:0,
+
+    ///  nofity  不需要网络框
+    __send: function(r,a, c, b, d,notify) {
+        this[b] = function() {
+            /// 回调函数
+            var cb=null;
+            //if(arguments[0] instanceof String) cb=arguments[0],arguments = arguments.splice(0,1);
+            //var ay = arguments;
+            //ay =  ay.splice(0,1);
+            var i = arguments[0] instanceof Object && (arguments[0].t && arguments[0].cb) ? (cb=arguments[0],1):0;
+            for (var e = {},
+                     f = 0; f < d.length; f++) e[d[f]] = arguments.length < f ? null: arguments[f+i] ;
+            if (d.length != arguments.length-i) throw "Message " + b + " arguments error. parameters is too short or too long.";
+
+            //e = {
+            //    pid: c,
+            //    seq:1,
+            //    data: JSON.stringify(e)
+            //};
+            //
+            //if (Server.IsOpened()) {
+            //    if ( this.onsent) this.onsent(e,notify );
+            //    a(e,cb,notify);
+            //    this.cachmsg = null
+            //} else this.cachmsg = e,
+            //    this.onfaildsend(e)
+            if(Server.state == 1)
+            {
+                if ( this.onsent) this.onsent(e,notify );
+                a(r,e,c,b,cb,notify);
+            }
+            else{
+                this.onfaildsend(e);
+            }
+        }
+    },
+    onfaildsend: function(a) {},
+    ondisconnect: function(a) {
+        cc.log("ondisconnect:"+ a?a:"")
+        this.state = 0;
+    },
+    onconnect: function(a) {
+        cc.log("onconnect:" + a.toString())
+    },
+    onerror: function(a) {
+        cc.log("onerror", a)
+        // a.fileName ? cc.log("net-Js-error: file:" + a.fileName, " line:" + a.lineNumber, " cols:" + a.columnNumber, "\n Url is " + this.ServerUrl + "\ne:" + a) : cc.log("net-Js-error: error ", a, "\n Url is " + this.ServerUrl)
+    },
+    init: function() {},
+    IsOpened: function() {
+        return ! 1
+    },
+    close: function(a) {
+        cc.log("close");
+        this.state = 0;
+    },
+    sendmsg: function() {},
+    beginConnect: function() {},
+    disconnect:function(){}
+});
