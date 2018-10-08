31ba3c6f6ed12780a78fba8c00af51f5664d763b
jianan <1072772483@qq.com>
2018-10-08 23:12:16 +0800

    this is a quick commit

--- server/game-server/app/gameData/agent/base/CBaseAgent.js
+++ server/game-server/app/gameData/agent/base/CBaseAgent.js
@@ -4,55 +4,51 @@
 require("../../../core/Core");
 
 Class({
-    ClassName:"Game.Agent.CBaseAgent",
-    IsMineAction:false,
-    MLastTime:0,
-    ctor:function(uid)
-    {
+    ClassName: "Game.Agent.CBaseAgent",
+    IsMineAction: false,
+    MLastTime: 0,
+    ctor: function (uid) {
         Object.defineProperty(this, "UserId", {
             get: function () {
                 return uid;
             }
         });
     },
-    Controller:{
-        value:false,
-        key:__controller,
-        get:function()
-        {
+
+    // 
+    Controller: {
+        value: false,
+        key: __controller,
+        get: function () {
             return this.__controller;
         },
-        set:function(v)
-        {
-            if(v != this.__controller)
-            {
+        set: function (v) {
+            if (v != this.__controller) {
                 this.__controller = v;
-                if(this.IsMineAction)
-                {
-                    if(v)
-                    {
+                if (this.IsMineAction) {
+                    if (v) {
                         this.Action();
                     }
-                    else
-                    {
+                    else {
                         this.RemoveController();
                     }
                 }
             }
         }
     },
-    Action:function()
-    {
+
+    // 
+    Action: function () {
 
     },
-    RemoveController:function()
-    {
+
+    // 
+    RemoveController: function () {
 
     }
 
 }).Static({
-    Create:function(uid)
-    {
+    Create: function (uid) {
         return new this(uid);
     }
 })
--- server/game-server/app/gameData/agent/zjh/CZJHAgent.js
+++ server/game-server/app/gameData/agent/zjh/CZJHAgent.js
@@ -1,26 +1,24 @@
 /**
  * Created by Class on 2017/3/22.
+ * 
+ * 扎金花游戏数据
  */
 require("../base/CBaseAgent");
 
 Class({
-    ClassName:"Game.Agent.CZJHAgent",
-    Base:"Game.Agent.CBaseAgent",
-    Room:null,
+    ClassName: "Game.Agent.CZJHAgent",  //
+    Base: "Game.Agent.CBaseAgent",      //基类名字
+    Room: null,
     IsMineAction:
     {
-        get:function()
-        {
+        get: function () {
             return this.Room.GamePersons[this.Room.CurrentActivity] == this.UserId;
         }
     },
-    Action:function()
-    {
+    Action: function () {
 
     },
-    RemoveController:function()
-    {
+    RemoveController: function () {
 
     }
-
 })
